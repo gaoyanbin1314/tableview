24892db2f8ac6c70e4eb4d432b9958d5acb7d2f8
Gaoyanbin <gaoyanbin1234@icloud.com>
2017-09-21 20:41:44 +0800

    小试tableview 封装

---
+++ TQTableViewCellRemoveController-master/README.md
@@ -0,0 +1,6 @@
+TQTableViewCellRemoveController
+===============================
+
+A table view cell remove animation with FBPOP
+
+![Image](https://raw.githubusercontent.com/TinyQ/TQTableViewCellRemoveController/master/READMEIMAGE/1.gif)
---
+++ TQTableViewCellRemoveController-master/TQTableViewCellRemoveController/TQCell.m
@@ -0,0 +1,33 @@
+//
+//  TQCell.m
+//  TQTableViewCellRemoveController
+//
+//  Created by qfu on 8/11/14.
+//  Copyright (c) 2014 qfu. All rights reserved.
+//
+
+#import "TQCell.h"
+
+@implementation TQCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+ 
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+}
+
+
+@end
